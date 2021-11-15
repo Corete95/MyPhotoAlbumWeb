4ad1ee9addc32a10815366abda936f4b1200dcd0
Corete95 <fanthell95@gmail.com>
2021-11-15 20:37:15 +0900

    [Add]Server 로그인유지 API 및 로그인 에러 수정

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -23,6 +23,7 @@ userRouter.post("/register", async (req, res) => {
       message: "user register",
       sessionId: session._id,
       name: user.name,
+      userId: user._id,
     });
   } catch (err) {
     res.status(400).json({ message: err.message });
@@ -32,6 +33,7 @@ userRouter.post("/register", async (req, res) => {
 userRouter.patch("/login", async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
+    if (!user) throw new Error("가입되지 않은 이메일입니다.");
     const isvalid = await compare(req.body.password, user.hashedPassword);
     if (!isvalid) throw new Error("입력하신 정보가 올바르지 않습니다.");
     user.sessions.push({ createdAt: new Date() });
@@ -41,6 +43,7 @@ userRouter.patch("/login", async (req, res) => {
       message: "user validated",
       sessionId: session._id,
       name: user.name,
+      userId: user._id,
     });
   } catch (err) {
     console.log(err);
@@ -56,6 +59,21 @@ userRouter.patch("/logout", async (req, res) => {
       { $pull: { sessions: { _id: req.headers.sessionid } } }
     );
     res.json({ message: "user is logged out" });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ message: err.message });
+  }
+});
+
+userRouter.get("/me", (req, res) => {
+  try {
+    if (!req.user) throw new Error("권한이 없습니다.");
+    res.json({
+      message: "success",
+      sessionId: req.headers.sessionid,
+      name: req.user.name,
+      userId: req.user._id,
+    });
   } catch (err) {
     console.log(err);
     res.status(400).json({ message: err.message });
